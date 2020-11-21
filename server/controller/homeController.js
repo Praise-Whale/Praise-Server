6c1e75dd64abc1b48f2d8a1b83fc7b6ee27dcfae
Choi Dain <twa05157@gmail.com>
2020-11-22 02:06:52 +0900

    API clear2

--- server/controller/homeController.js
+++ server/controller/homeController.js
@@ -2,25 +2,24 @@ const crypto = require('crypto');
 const util = require('../modules/util');
 const responseMessage = require('../modules/responseMessage');
 const statusCode = require('../modules/statusCode');
-const { Praise } = require('../models');
+const { praise } = require('../models');
 
 module.exports = {
     home : async (req, res) => {
 
         //1. 전체에서 name 하나 출력하기
         try {
-            const name = await Praise.findAll({
-            });
+            const name = await praise.findAll();
             //2. 칭찬을 random하게 구성하기
-            const praise = name.map(a => ([Math.random(), a]))
+            const praiseResult = name.map(a => ([Math.random(), a]))
             .sort((a, b) => a[0] - b[0])
             .map(a => a[1]);
 
             //3. status: 200, message: id, daily_praise, mission_praise 반환
             return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_USER_SUCCESS, {
-                id: praise.id,
-                daily_praise: praise.daily_praise,
-                mission_praise: praise.mission_praise,
+                id: praiseResult.id,
+                daily_praise: praiseResult.daily_praise,
+                mission_praise: praiseResult.mission_praise,
             }));
         } catch (error) {
             console.log(error);
