e82c082c5ac3712425e285bcd5208fe19022af64
Gyunny <wjdrbs966@naver.com>
2020-11-22 04:43:49 +0900

    UPDATE README.md

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 const { sequelize } = require('./models/index');
 
-sequelize.sync({ alter: true })
+sequelize.sync({ alter: false })
 .then(() => {
   console.log('데이터베이스 연결 성공');
 })
