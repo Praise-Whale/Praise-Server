3cf8ce3242b99a5d9f835a187988fa40007b42ed
Gyunny <wjdrbs966@naver.com>
2020-12-11 00:16:38 +0900

    Feat: 최근 칭찬 유저 조회

--- server/modules/responseMessage.js
+++ server/modules/responseMessage.js
@@ -10,11 +10,15 @@ module.exports = {
   NOT_FOUND_USER: "존재하지 않는 유저입니다",
   SINGIN_SUCCESS: "로그인 성공입니다",
 
+  // 최근 칭찬 유저 조회
+  LATELY_PRAISE_USER: "최근 칭찬 유저 조회 성공입니다",
+
+
   // TOKEN MESSAGE
   EMPTY_TOKEN : "토큰 값이 없습니다",
   EXPIRED_TOKEN : "토큰 값이 만료되었습니다",
   INVALID_TOKEN : "유효하지 않은 토큰값입니다",
   AUTH_SUCCESS : "인증에 성공했습니다",
   ISSUE_SUCCESS: "새로운 토큰이 생성되었습니다",
-  SUCCESS_TOKEN_REPLACEMENT: "토큰 재발급 했습니다"
+  SUCCESS_TOKEN_REPLACEMENT: "토큰 재발급 했습니다",
 }
--- server/routes/praise.js
+++ server/routes/praise.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const praiseController = require('../controller/praiseController');
+const jwtMiddlewares = require('../middlewares/middlewares');
 
-router.get('/target', praiseController.praiseTarget);
+router.get('/target', jwtMiddlewares.userJwt, praiseController.praiseTarget);
 
 module.exports = router;
