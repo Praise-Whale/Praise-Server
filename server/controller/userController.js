5594406bc3f78ce99c3786016bb22a076da11472
Gyunny <wjdrbs966@naver.com>
2020-12-10 23:12:45 +0900

    Feat: 회원가입 API 완료

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,3 +1,42 @@
 const statusCode = require("../modules/statusCode");
 const responseMessage = require("../modules/responseMessage");
 const util = require("../modules/util");
+const { user } = require('../models/index');
+const jwt = require('../modules/jwt');
+
+module.exports = {
+  signup: async (req, res) => {
+    const { nickName } = req.body;
+
+    if (!nickName) {
+      res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+      return;
+    }
+
+    const nickNameCheck = await user.findOne({
+      where: {
+        nickName: nickName
+      }
+    });
+
+    if (nickNameCheck) {
+      res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.ALREADY_NICKNAME));
+      return;
+    }
+
+    const userResult = await user.create({
+      nickName: nickName,
+      userLevel: 1,
+      alarmCheck: true
+    });
+
+    const { id } = userResult;
+
+    const { accessToken, refreshToken } = await jwt.sign(id);
+    
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SIGNUP_SUCCESS, {
+      accessToken: accessToken,
+      refreshToken: refreshToken
+    }))
+  }
+}
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controller/userController');
 
+router.post('/signup', userController.signup);
 
 module.exports = router;
