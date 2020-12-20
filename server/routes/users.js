81be7fa5503c9012063e4ab8c192b17cf4f2e827
Gyunny <wjdrbs966@naver.com>
2020-12-21 02:10:57 +0900

    update: post -> pust 변경

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,6 +9,6 @@ router.get('/home', jwtMiddlewares.userJwt, userController.userHome);
 router.post('/alarm', jwtMiddlewares.userJwt, userController.alaramCheck);
 router.get('/check/:nickName', userController.nickNameCheck);
 router.put('/nickname', jwtMiddlewares.userJwt, userController.nickNameChange);
-router.post('/token', jwtMiddlewares.refreshToken);
+router.put('/token', jwtMiddlewares.refreshToken);
 
 module.exports = router;
