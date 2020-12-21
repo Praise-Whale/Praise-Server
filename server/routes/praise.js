84b153f7a034f845ddb65d14141d1171779fb28d
Gyunny <wjdrbs966@naver.com>
2020-12-21 16:34:24 +0900

    update: 칭찬한 사람 URI 변경

--- server/routes/praise.js
+++ server/routes/praise.js
@@ -5,6 +5,7 @@ const jwtMiddlewares = require('../middlewares/middlewares');
 
 router.get('/target', jwtMiddlewares.userJwt, praiseController.praiseTarget);
 router.get('/collection', jwtMiddlewares.userJwt, praiseController.praiseCollection);
-router.post('/praiser/:praiseId', jwtMiddlewares.userJwt, praiseController.praiserUp);
+router.post('/:praiseId', jwtMiddlewares.userJwt, praiseController.praiserUp);
+
 
 module.exports = router;
