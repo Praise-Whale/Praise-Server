869de053a992275d87e627ae4aee0207084e7cc7
Choi Dain <twa05157@gmail.com>
2021-02-14 01:55:06 +0900

    Chore: praised 주석 삭제와 기존에 있던 관계 주석 추가

--- server/models/index.js
+++ server/models/index.js
@@ -16,16 +16,12 @@ db.Sequelize = Sequelize;
 db.praise = require('./praise')(sequelize, Sequelize);
 db.praiseTarget = require('./praiseTarget')(sequelize, Sequelize);
 db.user = require('./user')(sequelize, Sequelize);
-//db.isPraised = require('./isPraised')(sequelize, Sequelize);
 
-
-/** 1 : 1   Praise : P */
+/** 1 : 1   Praise : PraiseTarget */
 db.praise.hasOne(db.praiseTarget, { onDelete: 'cascade' });
 db.praiseTarget.belongsTo(db.praise);
 
-//db.user.belongsToMany(db.praise, { through: 'isPraised', as: 'praised' })
-//db.praise.belongsToMany(db.user, { through: 'isPraised', as: 'praiser'})
-
+/** 1 : N   User : PraiseTarget */
 db.user.hasMany(db.praiseTarget, { ondDelete: 'cascade' });
 db.praiseTarget.belongsTo(db.user, { onDelete: 'cascade' });
 
