8db979a1254193e677c4bd790defdcd64a8ec41e
Gyunny <wjdrbs966@naver.com>
2021-03-07 02:43:07 +0900

    Fix: 칭찬 랭킹 조회시 칭찬 한 수 중복 제거 (#60)

--- server/models/query/users.js
+++ server/models/query/users.js
@@ -2,7 +2,7 @@ const pool = require('../../modules/pool');
 
 const test = {
   userRanking: async (userIdx) => {
-    const query = `SELECT COUNT(praisedName) as praiseCount FROM praiseTarget WHERE userId = ${userIdx}`;
+    const query = `SELECT COUNT(DISTINCT praisedName) as praiseCount FROM praiseTarget WHERE userId = ${userIdx}`;
     try {
       const result = await pool.queryParam(query);
       return result;
