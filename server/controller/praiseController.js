0244ce247b14fe52a4e0707c31edc6d6e3813b8c
Choi Dain <twa05157@gmail.com>
2021-03-08 01:28:11 +0900

    Fix: 칭찬 카드 조회

--- server/controller/praiseController.js
+++ server/controller/praiseController.js
@@ -65,26 +65,26 @@ module.exports = {
     const { year, month } = req.params;
 
     try {
-      if (month === 0) {
+      if (month == 0) {
         const monthPraiseCount = await praise.userMonthPraiseCount(year, userIdx);
+        const praiseCount = monthPraiseCount[0].praiseCount;
         const wholePraise = await praise.userWholePraise(year, userIdx);
 
         return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.PRAISE_ALL_COLLECTION, {
-          monthPraiseCount,
-          collectionPraise
+          praiseCount,
+          wholePraise
         }));
       }
 
-      const yearPraiseCount = await praise.userYearPraiseCount(year, month, userIdx);
-  
-      const collectionPraise = await praise.userYearWholePraise(year, month, userIdx);
-
-      const praiseCount = yearPraiseCount[0].praiseCount;
-      
-      return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.PRAISE_ALL_COLLECTION, {
-        praiseCount,
-        collectionPraise
-      }));
+      else {
+        const yearPraiseCount = await praise.userYearPraiseCount(year, month, userIdx);
+        const praiseCount = yearPraiseCount[0].praiseCount;
+        const collectionPraise = await praise.userYearWholePraise(year, month, userIdx);
+        return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.PRAISE_YEAR_MONTH_COLLECTION, {
+          praiseCount,
+          collectionPraise
+        }));
+      }
 
     } catch (err) {
       res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
