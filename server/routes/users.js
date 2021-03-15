1f5be8fc428ced2371f8ce8d4b3b036770c3d68e
Gyunny <wjdrbs966@naver.com>
2021-03-15 17:01:59 +0900

    Feat: FCM add & 회원가입 API 수정

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userController');
+const alarmController = require('../controller/alarmController');
 const jwtMiddlewares = require('../middlewares/middlewares');
 
 router.post('/signup', userController.signup);
@@ -10,5 +11,6 @@ router.post('/alarm', jwtMiddlewares.userJwt, userController.alaramCheck);
 router.get('/check/:nickName', userController.nickNameCheck);
 router.put('/nickname', jwtMiddlewares.userJwt, userController.nickNameChange);
 router.put('/refreshtoken', jwtMiddlewares.refreshToken);
+router.get('/alarm', alarmController.alarm);
 
 module.exports = router;
