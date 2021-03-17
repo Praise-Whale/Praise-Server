a3ebe32905928bcab043c7de2719a7fa522d9725
Gyunny <wjdrbs966@naver.com>
2021-03-17 23:49:24 +0900

    Update: 알람 시간

--- server/controller/praiseController.js
+++ server/controller/praiseController.js
@@ -14,7 +14,7 @@ const rule = new schedule.RecurrenceRule();
 rule.tz = "Asia/Seoul";
 
 rule.hour = 23;
-rule.minute = 30;
+rule.minute = 55;
 rule.second = 0;
 
 const sch = schedule.scheduleJob(rule, async () => {
